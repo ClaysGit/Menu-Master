fe112d3b2379a5df01072e7a894f7c95599be47f
joshjdhf <joshjdhf@gmail.com>
2014-06-04 14:53:29 -0400

    very basic windows service project
    Just the basic service template with a few properties changed. install
    from command line with installutil.exe menumaster.exe, in the
    /bin/release folder. uninstall with installutil.exe /u

---
+++ basic windows service project/Service1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuMaster
+{
+    public partial class MenuMaster : ServiceBase
+    {
+        public MenuMaster()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
